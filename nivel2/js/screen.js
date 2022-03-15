50688aa446f2d3f5e54db93f8994f55c4fc22833
Reynaldo-byte <64993522+Reynaldo-byte@users.noreply.github.com>
2022-03-15 14:39:08 -0500

    Add files via upload

---
+++ nivel2/js/screen.js
@@ -0,0 +1,81 @@
+/**
+ * Script para pintar el tablero
+ */
+
+'use strict'
+const screenDraw=[
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [5,1,4,1,1,1,1,4,1,0,1,1,1,1,1,1,1,0,0],
+    [0,1,0,0,1,0,0,0,1,0,1,0,0,0,1,0,0,4,0],
+    [0,1,1,1,1,4,4,4,0,1,1,1,1,1,1,1,1,0,0],
+    [0,0,0,0,1,0,1,0,0,0,0,0,1,0,1,0,0,1,0],
+    [0,4,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,1,0],
+    [0,0,1,0,1,0,0,0,0,0,1,0,0,0,4,0,0,1,0],
+    [0,0,1,0,1,0,1,1,1,1,1,1,1,0,1,0,0,1,0],
+    [0,1,1,0,0,0,1,0,0,0,0,0,1,0,1,0,0,1,0],
+    [0,1,1,1,1,1,1,0,3,1,1,0,1,1,1,1,1,1,0],
+    [0,0,0,0,1,0,1,0,0,0,0,0,1,0,1,0,0,0,0],
+    [0,0,0,0,1,0,1,1,1,1,1,1,1,0,1,0,0,0,0],
+    [0,0,0,0,1,0,1,0,0,0,0,0,1,0,1,0,0,0,0],
+    [0,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,0],
+    [0,4,0,0,1,0,0,0,1,0,1,0,0,0,1,0,0,1,0],
+    [0,1,1,0,1,1,1,1,1,1,1,1,1,1,1,0,1,1,0],
+    [0,0,1,0,1,0,1,0,0,0,0,0,1,0,1,0,1,0,0],
+    [0,1,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,1,0],
+    [0,4,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,1,0],
+    [0,1,4,1,1,1,0,1,1,4,1,1,1,1,1,4,1,1,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+];
+
+const board=document.querySelector("#board");
+
+createBoard();
+function createBoard() {
+    let j=Math.floor(Math.random()*3)
+    // dibujamos el tablero
+    screenDraw.map(line => {
+        line.map((el, i) => {
+            const div=document.createElement("div");
+            if (i==0) {
+                div.classList.add("newLine");
+            }
+            if (el==0) {
+                div.classList.add("wall");
+            } else if (el==2) {
+                div.classList.add("door");
+            } else if (el==1) {
+                j=Math.floor(Math.random()*3)
+                if(j==0){
+                div.classList.add("sandia");
+                }
+                else if(j==1){
+                     div.classList.add("jamon");
+                }
+                    else if(j==2){
+                         div.classList.add("brocoli");
+                    }
+               
+            } else if (el==9) {
+                div.classList.add("energizer")
+            }else if(el==4){
+                    j=Math.floor(Math.random()*4)
+                if(j==0){
+                div.classList.add("chatarra");
+                }
+                else if(j==1){
+                     div.classList.add("perro");
+                }
+                    else if(j==2){
+                         div.classList.add("helado");
+                    }
+                    else if(j==3){
+                         div.classList.add("dona");
+                    }
+            }
+            else if(el==5){
+                div.classList.add("casa")
+            }
+            board.appendChild(div);
+        });
+    });
+}
---
+++ nivel2/script.js
@@ -0,0 +1,73 @@
+
+var dejado;
+function allowDrop(ev) {
+  ev.preventDefault();
+  dejado=ev.target.id;
+      
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+  console.log(ev.target.id) 
+}
+var imge=9;
+var imgr=10;
+function drop(ev) {
+ 
+  ev.preventDefault();
+  var content = ev.dataTransfer.getData("text");
+  var drag = document.getElementById(content);
+  var drop = document.getElementById(dejado);
+  var src = drag.getAttribute("src");
+    var svg = document.getElementById("Capa_1");
+  var img = document.createElement("image");
+  var id=drag.getAttribute('id')
+ console.log(dejado) 
+try {
+
+
+
+
+} catch (error) {
+  console.error(error);
+  // expected output: ReferenceError: nonExistentFunction is not defined
+  // Note - error messages will vary depending on browser
+}
+
+
+  if (dejado === drag.getAttribute("data-drag")) {
+    imge=imge-1;
+    imgr=imgr-1;
+          if (imgr==0 ) {
+            console.log('a')
+  document.getElementById('mensaje').removeAttribute('hidden')
+}
+    console.log(imgr);
+    drag.remove();
+
+    switch (dejado) {
+  
+      case "sano":
+      disB(id)
+      break;
+      case "insano":
+       disB(id)
+      break;
+      
+
+
+      
+
+    }
+  }
+}
+
+function disB(id) {
+    document.getElementById(id).classList.remove("dis-n");
+  document.getElementById(id).classList.add("dis-b");
+
+}
+function disN(id) {
+    document.getElementById(id).classList.remove("dis-b");
+  document.getElementById(id).classList.add("dis-n");
+}
